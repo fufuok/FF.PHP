ee24df2d20401c754f116fc3a24fbec8143d264d
Fufu <fufuok@qq.com>
2019-08-04 10:53:14 +0800

    Update: libs

--- web/ff/l/function.php
+++ web/ff/l/function.php
@@ -1056,6 +1056,31 @@ function get_str($str, $length, $suffix = '..')
     return $new;
 }
 
+// 获取文件 MIME 类型
+function get_file_mime_type($file, $encoding = true)
+{
+    if (function_exists('finfo_file')) {
+        $finfo = finfo_open(FILEINFO_MIME);
+        $mime = @finfo_file($finfo, $file);
+        finfo_close($finfo);
+    } else {
+        if (substr(PHP_OS, 0, 3) == 'WIN') {
+            $mime = @mime_content_type($file);
+        } else {
+            $file = escapeshellarg($file);
+            $mime = @exec("file --mime -b {$file}");
+        }
+    }
+    if (!$mime) {
+        return '';
+    }
+    if ($encoding) {
+        return $mime;
+    }
+
+    return substr($mime, 0, strpos($mime, '; '));
+}
+
 // 取文件扩展名
 function get_fileext($name)
 {
@@ -1066,6 +1091,41 @@ function get_fileext($name)
 function mk_uniqid($pre = '', $md5 = 0)
 {
     return $pre . ($md5 ? md5(uniqid(mt_rand(0, 999999), true) . random(8)) : uniqid()) . strtolower(random(3));
+}
+
+// 生成 GUID
+function mk_guid($trim = true)
+{
+    // Windows
+    if (function_exists('com_create_guid') === true) {
+        return $trim === true ? trim(com_create_guid(), '{}') : com_create_guid();
+    }
+
+    // OSX/Linux
+    if (function_exists('openssl_random_pseudo_bytes') === true) {
+        $data = openssl_random_pseudo_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // set bits 6-7 to 10
+        $guidv4 = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+
+        return $trim ? $guidv4 : '{' . $guidv4 . '}';
+    }
+
+    // Other
+    mt_srand((double)microtime() * 10000);
+    $charid = strtolower(md5(uniqid(rand(), true)));
+    $hyphen = chr(45);                  // "-"
+    $lbrace = $trim ? '' : chr(123);    // "{"
+    $rbrace = $trim ? '' : chr(125);    // "}"
+    $guidv4 = $lbrace .
+        substr($charid,  0,  8) . $hyphen .
+        substr($charid,  8,  4) . $hyphen .
+        substr($charid, 12,  4) . $hyphen .
+        substr($charid, 16,  4) . $hyphen .
+        substr($charid, 20, 12) .
+        $rbrace;
+
+    return $guidv4;
 }
 
 // 文件名生成，前缀避免多台服务器生成文件重复，md5 唯一性更强
